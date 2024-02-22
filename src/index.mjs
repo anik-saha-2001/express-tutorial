bcc6a3abe9783bf8112f9a9eefc9ebb63655857b
anik-saha-2001 <aniksahamda@gmail.com>
2024-02-22 20:20:21 +0530

    cookies learnt

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser("helloworld"));
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
@@ -14,5 +16,7 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req, res) => {
+  //cookie for 1hr
+  res.cookie("hello", "world", { maxAge: 10000, signed: true });
   res.status(201).send({ msg: "Hello There!" });
 });
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,10 +3,18 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (request, response) => {
-  response.send([
-    { pid: 10, name: "MacBook" },
-    { pid: 20, name: "Gaming Computer" },
-  ]);
+  console.log(request.headers.cookie);
+  console.log(request.cookies);
+  console.log(request.signedCookies);
+  if (request.signedCookies.hello && request.signedCookies.hello === "world")
+    return response.send([
+      { pid: 10, name: "MacBook" },
+      { pid: 20, name: "Gaming Computer" },
+    ]);
+
+  return response
+    .status(403)
+    .send({ msg: "Sorry you need the correct signed cookie" });
 });
 
 export default router;
